0155339f0fc1194e84a4a5b77c14329799dc42bc
João Vitor Canola Raposo <joao.raposo@etec.sp.gov.br>
2023-03-10 08:50:45 -0300

    criado função index

--- itapira/enquete/views.py
+++ itapira/enquete/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1>olha se voce nao me ama entao nao me ligue</h1><h2>Caneta.azul,azul caneta</h2>")
